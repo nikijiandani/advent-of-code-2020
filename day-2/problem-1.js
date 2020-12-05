ac38756cef7103161af2e4e88961a07a11f8c584
Nikita Jiandani <niki.jiandani@gmail.com>
2020-12-05 11:53:11 -0500

    Solution to Day 2 problem 1

--- day-2/problem-1.js
+++ day-2/problem-1.js
@@ -4,11 +4,27 @@ fs.readFile("./input.txt", "utf8", (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
-    // const arr = data.split("\n").map((item) => item.split(" "));
+    const arr = data.trim().split("\n").map((item) => item.split(" "));
+    let valid_password_count = 0
 
-    // for (item in arr) {
-    //   console.log(item);
-    // }
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      const valid_letter = element[1].slice(0, 1)
+      const password = [...element[2]]
+
+       // this counter keeps track of how many times the letter was repeated
+      let counter = 0;
+      for (let j = 0; j < password.length; j++) {
+        const element = password[j];
+        if(element === valid_letter) counter++
+      }
+      const policy_min = parseInt(element[0].split('-')[0], 10)
+      const policy_max = parseInt(element[0].split('-')[1], 10)
+
+      if((counter >= policy_min) && (counter <= policy_max)) {
+        valid_password_count++
+      }
+    }
+    console.log({valid_password_count})
   }
 });
